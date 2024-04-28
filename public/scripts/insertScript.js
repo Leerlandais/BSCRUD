8508d64d055b7d5c151f5e1ed0249262d46d9842
leerlandais <leerlandais@gmail.com>
2024-04-28 19:11:32 +0200

    tidied JS

--- public/scripts/insertScript.js
+++ public/scripts/insertScript.js
@@ -1,5 +1,7 @@
 // Voici les contrôles pour l'insertion
-// 
+// Les fonctions pour l'insertion et la mise à jour sont presque identiques. Je devrais trouver comment les combiner, mais ce sera pour demain
+// Il y a quelques jours, André a aligné tous les "="
+// C'est génial. Le code est beaucoup plus clair
 
 if (document.getElementById("insertForm")) {
 const insertForm    = document.getElementById("insertForm"),
@@ -29,13 +31,14 @@ const insertForm    = document.getElementById("insertForm"),
     }
     
 }  
+
  
 if (document.getElementById("updateForm")) {
-    const updateForm    = document.getElementById("updateForm"),
-          nomInpUpdate        = document.getElementById("nomInpUpdate"),
-          descInpUpdate       = document.getElementById("descInpUpdate"),
-          latInpUpdate        = document.getElementById("latInpUpdate"),
-          lonInpUpdate        = document.getElementById("lonInpUpdate");
+const updateForm    = document.getElementById("updateForm"),
+      nomInpUpdate        = document.getElementById("nomInpUpdate"),
+      descInpUpdate       = document.getElementById("descInpUpdate"),
+      latInpUpdate        = document.getElementById("latInpUpdate"),
+      lonInpUpdate        = document.getElementById("lonInpUpdate");
     
 
 updateForm.addEventListener("submit", function(event) {
@@ -44,11 +47,11 @@ updateForm.addEventListener("submit", function(event) {
 });
 
 function validateInputsForUpdate () {
-    insertError.textContent = ""
-   let nom  = nomInpUpdate.value;
-   let desc = descInpUpdate.value;
-   let lat  = latInpUpdate.value;
-   let lon  = lonInpUpdate.value;
+       insertError.textContent = ""
+       let nom  = nomInpUpdate.value;
+       let desc = descInpUpdate.value;
+       let lat  = latInpUpdate.value;
+       let lon  = lonInpUpdate.value;
 
     (nom == "" || desc == "" || lat == "" || lon == "") || (isNaN(lat) || isNaN(lon)) ?
         insertError.textContent = "Remplissez correctement les champs" :
